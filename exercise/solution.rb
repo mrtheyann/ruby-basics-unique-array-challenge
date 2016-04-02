6950dce1c4282a2bb06d0cf000758fae6f5620ed
fox <ctrl-break@mail.ru>
2016-04-02 12:55:08 +0400

    solution v0.1

--- exercise/solution.rb
+++ exercise/solution.rb
@@ -4,7 +4,28 @@ Wrong.config.color
 
 a = [1,3,7,3,2,5,1,1]
 b = [2,3,5,5,12]
+c = []
+d = [5, 5, 5, 5, 5, 0]
 
-a = a.sort
+def unique_array(arr)
+#BEGIN
+arr = arr.sort
+i = 0
+if arr.length > 1
+  while i <= arr.length-1
+    if arr[i] == arr[i+1]
+      arr.delete_at(i+1)
+    else
+      i += 1
+    end
+  end
+end
+return arr
 
-assert { a == [1, 1, 1,2,3,3,5,7] }
+#END
+end
+
+assert { unique_array(a) == [1,2,3,5,7] }
+assert { unique_array(b) == [2,3,5,12] }
+assert { unique_array(c) == [] }
+assert { unique_array(d) == [0, 5] }
